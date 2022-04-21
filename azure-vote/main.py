c3e732e7dba51fe3cd6f11685cb0b94fce1079b8
xDestyn <omar.flores.cs@outlook.com>
2022-04-20 22:54:19 -0500

    Add event handler

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -26,13 +26,20 @@ view_manager = stats.view_manager
 config_integration.trace_integrations(['logging'])
 config_integration.trace_integrations(['requests'])
 
+# Handler
+handler = AzureLogHandler(connection_string='InstrumentationKey=706a869a-1769-4b48-8bd9-d46e98f2f698;IngestionEndpoint=https://centralus-2.in.applicationinsights.azure.com/;LiveEndpoint=https://centralus.livediagnostics.monitor.azure.com/quickpulseservice.svc')
+handler.setFormatter(logging.Formatter('%(traceId)s %(spanId)s %(message)s'))
+
 # Logging
 # TODO: Setup logger
 logger = logging.getLogger(__name__)
 
-handler = AzureLogHandler(connection_string='InstrumentationKey=706a869a-1769-4b48-8bd9-d46e98f2f698;IngestionEndpoint=https://centralus-2.in.applicationinsights.azure.com/;LiveEndpoint=https://centralus.livediagnostics.monitor.azure.com/quickpulseservice.svc')
-handler.setFormatter(logging.Formatter('%(traceId)s $(spanId)s $(message)s'))
+# Log Handler
 logger.addHandler(handler)
+
+# Event Handler
+logger.addHandler(AzureEventHandler(connection_string='InstrumentationKey=706a869a-1769-4b48-8bd9-d46e98f2f698;IngestionEndpoint=https://centralus-2.in.applicationinsights.azure.com/;LiveEndpoint=https://centralus.livediagnostics.monitor.azure.com/quickpulseservice.svc'))
+
 logger.setLevel(logging.INFO)
 
 
